d7102a25f0b5c5519144aa008a6022c0e6060b62
joseph <joseph.ketterer@gmail.com>
2025-01-31 14:45:41 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,32 @@ import HomePage from "./HomePage";
 import { getData } from "@/lib/getData";
 
 
+async function getData1() {
+
+  // return await (await get(ref(database))).val()
+
+  const DB_URL =  'https://api.jsonbin.io/v3/b/679cd841e41b4d34e481cb2d'
+  const res = await fetch(DB_URL, {
+		method: 'GET',
+		headers: {
+			'X-Master-Key': process.env.X_MASTER_KEY as string,
+			'X-Access-Key': process.env.X_ACCESS_KEY as string
+		},
+	})
+  const data = res.json()
+  return data
+}
+
 export default async function page() {
-  const data = await getData();
+  const data = await getData1();
+
+
   console.log(data)
 
   return (
     <>
       {data ? (
-        <HomePage data={data} />
+        <HomePage data={data.record} />
       ) : (
         <div className="h-screen w-screen flex flex-col items-center justify-center gap-5 text-blue-600 fixed z-30 bg-gray-100 dark:bg-grey-900">
           {/* <FaNodeJs size={100} className="animate-pulse" /> */}
