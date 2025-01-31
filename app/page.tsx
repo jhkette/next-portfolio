6db080fdaa14697b27b33bf1f63f5aa6a8da4fd3
joseph <joseph.ketterer@gmail.com>
2025-01-31 12:28:39 +0000

    firebase done

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-import { FaNodeJs } from "react-icons/fa"
-import HomePage from "./HomePage"
+import { FaNodeJs } from "react-icons/fa";
+import HomePage from "./HomePage";
 // import { ref, get } from "firebase/database"
-// import { database } from "@/firebase"
+import { ref, onValue } from "firebase/database";
+import { database } from "@/lib/firebase";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/data.json", { cache: "no-store" });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const data = await res.json();
+  const dataRef = ref(database, "/");
+  let data;
+  onValue(dataRef, (snapshot) => {
+    data = snapshot.val();
+  });
   return data;
 }
 export default async function page() {
-
-  const data = await getData()
+  const data = await getData();
 
   return (
     <>
-      {data ?
+      {data ? (
         <HomePage data={data} />
-        :
-        <div className='h-screen w-screen flex flex-col items-center justify-center gap-5 text-blue-600 fixed z-30 bg-gray-100 dark:bg-grey-900'>
-          <FaNodeJs size={100} className='animate-pulse' />
-          <p className='animate-pulse text-xl'>Loading...</p>
+      ) : (
+        <div className="h-screen w-screen flex flex-col items-center justify-center gap-5 text-blue-600 fixed z-30 bg-gray-100 dark:bg-grey-900">
+          <FaNodeJs size={100} className="animate-pulse" />
+          <p className="animate-pulse text-xl">Loading...</p>
         </div>
-      }
+      )}
     </>
-  )
+  );
 }
---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL, // This is crucial for Realtime Database
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
