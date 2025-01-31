65b6aa6e7ce3aadd2030df57caca7c79fd54d6c5
joseph <joseph.ketterer@gmail.com>
2025-01-31 18:10:50 +0000

    datats

---
+++ lib/data.ts
@@ -0,0 +1,100 @@
+export const data = {
+    main: {
+      name: "Joseph",
+      shortDesc: "I focus on developing user-friendly web applications that meet the client's requirements, with attention to detail, scalability, and performance.",
+      titles: ["Frontend Development", "Backend Development"],
+      heroImage: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+      techStackImages: [
+        "https://img.icons8.com/color/144/000000/material-ui.png",
+        "https://img.icons8.com/color/144/000000/html-5--v1.png",
+        "https://img.icons8.com/color/144/000000/redux.png",
+        "https://img.icons8.com/color/144/000000/chakra-ui.png"
+      ]
+    },
+    about: {
+      aboutImage: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+      aboutImageCaption: "< I build stuff 🚀/>",
+      title: "Full Stack Developer",
+      about: "I am a Full-Stack developer based in Pune, India. I'm an Information Technology undergraduate from SPPU, and my journey in web development began during my first year of college. I'm passionate about creating beautiful, functional, and user-friendly websites and applications, and I'm constantly pushing myself to learn and grow as a developer. Love building full-stack clones and side projects.",
+      callUrl: "",
+      resumeUrl: ""
+    },
+    socials: [
+      { 
+        name: "Github",
+        icon: "FaGithub",
+        link: "https://github.com/jhon-doe"
+      }
+    ],
+    skills: [
+      { name: "JavaScript", image: "https://img.icons8.com/color/144/null/javascript--v1.png", category: "Frontend" },
+      { name: "Firebase", image: "https://img.icons8.com/color/144/null/firebase.png", category: "Backend" },
+      { name: "Typescript", image: "https://img.icons8.com/color/144/null/typescript--v1.png", category: "Frontend" },
+      { name: "PHP", image: "https://img.icons8.com/color/144/null/php.png", category: "Backend" },
+      { name: "Python", image: "https://img.icons8.com/color/144/null/python.png", category: "Backend" },
+      { name: "Java", image: "https://img.icons8.com/?size=100&id=13679&format=png&color=000000", category: "Backend" },
+      { name: "SQL", image: "https://img.icons8.com/color/144/null/sql.png", category: "Backend" },
+      { name: "GitHub", image: "https://img.icons8.com/material-outlined/96/null/github.png", category: "Tools" },
+      { name: "Docker", image: "https://img.icons8.com/color/docker", category: "Tools" },
+      { name: "Git", image: "https://img.icons8.com/color/git", category: "Tools" }
+    ],
+    projects: [
+      {
+        name: "Project Title",
+        techstack: "HTML5, CSS3, JavaScript, jQuery",
+        category: "MERN Stack",
+        image: "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210114225740/10-Best-Web-Development-Project-Ideas-For-Beginners-in-2021.png",
+        links: { code: "", video: "", visit: "" }
+      },
+      {
+        name: "Project Title",
+        techstack: "Flutter, Dart, Firebase",
+        category: "Flutter",
+        image: "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210114225740/10-Best-Web-Development-Project-Ideas-For-Beginners-in-2021.png",
+        links: { code: "", video: "", visit: "" }
+      },
+      {
+        name: "Project Title",
+        techstack: "PHP, MySQL, HTML5, CSS3, Bootstrap, JavaScript",
+        category: "LAMP Stack",
+        image: "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210114225740/10-Best-Web-Development-Project-Ideas-For-Beginners-in-2021.png",
+        links: { code: "", video: "", visit: "" }
+      }
+    ],
+    educations: [
+      { institute: "XYZ School | CBSE", degree: "HSC Science", duration: "2014 - 2016" },
+      {
+        institute: "XYZ College Of Engineering",
+        degree: "B.Tech CSE",
+        duration: "2016 - 2020",
+        desc: [
+          "Pursuing IT Engineering course from ABC University",
+          "Worked on web application development using HTML, CSS, and JavaScript, designing a database system using SQL",
+          "Familiar with software development methodologies and project management practices, including Agile and Waterfall methodologies."
+        ]
+      }
+    ],
+    experiences: [
+      {
+        company: "ABC Tech Solutions",
+        position: "Web Developer",
+        duration: "2020 -2022",
+        desc: [
+          "Assisted with design and development of project.",
+          "Worked on building the product from scratch and built multiple dashboards.",
+          "Skills: HTML · CSS · JavaScript · TailwindCSS · ReactJS · NodeJS · MySQL · REST APIs"
+        ]
+      },
+      {
+        company: "XYZ Tech Solutions",
+        position: "Web Developer",
+        duration: "2022 - present",
+        desc: [
+          "Assisted with design and development of project.",
+          "Worked on building the product from scratch and built multiple dashboards.",
+          "Skills: HTML · CSS · JavaScript · TailwindCSS · ReactJS · NodeJS · MySQL · REST APIs"
+        ]
+      }
+    ]
+  };
+  
