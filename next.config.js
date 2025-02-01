a690465e135ebdd0bfee3f7e2fca24c431b1f20d
joseph <joseph.ketterer@gmail.com>
2025-02-01 18:27:21 +0000

    test sanity

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+ 
   images: {
     domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org']
   }
