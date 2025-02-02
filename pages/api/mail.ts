5366f4928c1c0b6855c85fdcc3beced292b9f31a
joseph <joseph.ketterer@gmail.com>
2025-02-02 08:38:54 +0000

    nodemailer fix

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -9,6 +9,8 @@ type Data = {
   message: string;
 };
 
+// https://stackoverflow.com/questions/65631481/nodemailer-in-vercel-not-sending-email-in-production
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -26,9 +28,31 @@ export default async function handler(
       subject: `${name.toUpperCase()} sent you a message from Portfolio`,
       text: message,
     };
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error: Error, success: any) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
     try {
-      const info = await transporter.sendMail(mailOptions);
-      console.log("Email sent:", info);
+      await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailOptions, (err: Error, info: any) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          } else {
+            console.log(info);
+            resolve(info);
+          }
+        });
+      });
 
       return res.json({ message: "Email successfully sent" });
     } catch (err) {
