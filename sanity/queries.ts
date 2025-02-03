7f67e86d56f15e22f59e590f07f7cc997c0637f6
joseph <joseph.ketterer@gmail.com>
2025-02-03 11:03:28 +0000

    ts-ignore next studio

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -1,6 +1,12 @@
-import { client } from "./lib/client";
+
 import { defineQuery } from "next-sanity";
 export const POSTS_QUERY = 
 defineQuery(`*[_type == "post" && defined(slug.current)]|order(publishedAt desc){
 _id, title, body, slug, publishedAt,  author->{name}, 
+}`);
+
+
+export const DESCRIPTION_QUERY = 
+defineQuery(`*[_type == "description" && name == "joseph"]{
+_id, name, description, 
 }`);
