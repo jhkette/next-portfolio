fa3b738fd452051a5069f37fd3bb46eb36ef8c7f
joseph <joseph.ketterer@gmail.com>
2025-02-05 13:18:29 +0000

    change order

--- components/experiences/Experiences.tsx
+++ components/experiences/Experiences.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 const Experiences = ({   educationSanity, experienceSanity }: Props) => {
 
-    const [show, setShow] = useState("Experience")
+    const [show, setShow] = useState("Education")
     const [viewAll, setViewAll] = useState(false)
     const [experiences, setExperiences] = useState([...experienceSanity].reverse() as experience[])
     const [educations, setEducations] = useState([...educationSanity].reverse() as education[])
@@ -23,7 +23,7 @@ const Experiences = ({   educationSanity, experienceSanity }: Props) => {
             <h2 className="text-4xl text-center">Experience</h2>
 
             <div className="w-fit mx-auto mt-6 p-2 bg-white dark:bg-grey-800 rounded-md flex gap-2 items-center">
-                {['Experience', 'Education'].map((e, i) => (
+                {[ 'Education', 'Experience'].map((e, i) => (
                     <button key={i} onClick={() => setShow(e)} className={`py-2 px-4 rounded-md transition-colors ${show === e ? 'bg-blue-600 text-white' : 'hover:bg-gray-100 hover:dark:bg-grey-900 text-black dark:text-white'}`}>{e}</button>
                 ))
                 }
@@ -34,7 +34,7 @@ const Experiences = ({   educationSanity, experienceSanity }: Props) => {
                     <div className="left-6 md:left-1/2 absolute border-opacity-20 border-gray-400 dark:border-grey-800 h-full border"></div>
 
                     {viewAll ?
-                        (show === "Experience" ? experiences : educations).map((e, i) => (
+                        (show === "Experience" ? educations : experiences).map((e, i) => (
                             // @ts-ignore
                             <ExperienceCard key={i} {...e} index={i} />
                         ))
