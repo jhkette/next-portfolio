1445739528fe2693a0967882fed365e57c6f16a7
joseph <joseph.ketterer@gmail.com>
2025-03-11 12:02:18 +0000

    a

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -62,7 +62,7 @@ const ContactForm = () => {
                 <Image unoptimized={true} quality={100} alt="contact" src="/drawnenvelope.png" className="hidden md:block w-2/6 h-full object-cover" width={1000} height={1000} />
                 <div className="w-full md:w-1/2">
                     <h3 className="text-2xl">Get in touch</h3>
-                    <p className="text-gray-400 mb-4 text-sm md:text-base">My inbox is always open! 💌 Whether you&apos;ve got a burning question or want to drop a friendly &quot;hello&quot;, I&apos;m all ears!👂 Let&apos;s chat! 🎉</p>
+                    <p className="text-gray-400 mb-4 text-sm md:text-base">My inbox is always open! Let me know if you have a query! 🎉</p>
 
                     <form onSubmit={handleSubmit} className="flex flex-col gap-4 rounded-xl">
                         <input onChange={handleChange} required value={values.name} name="name" type="text" placeholder='Full Name *' className="outline-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" />
